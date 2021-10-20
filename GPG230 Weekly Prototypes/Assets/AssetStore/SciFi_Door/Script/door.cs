713dcbfffb2e2ece97c4ef0bfde33423d695c5cb
ConnorYounger <72112139+ConnorYounger@users.noreply.github.com>
2021-10-20 18:39:20 +1100

    Story level 1 reverb

--- GPG230 Weekly Prototypes/Assets/AssetStore/SciFi_Door/Script/door.cs
+++ GPG230 Weekly Prototypes/Assets/AssetStore/SciFi_Door/Script/door.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using System.Collections;
 
-public class door : MonoBehaviour {
+public class door : MonoBehaviour 
+{
 	public Animation thedoor;
+	public AudioSource audioSource;
 
-    void OnTriggerEnter ( Collider obj  ){
-	thedoor.Play("open");
-}
+    void OnTriggerEnter ( Collider obj  )
+	{
+		thedoor.Play("open");
 
-void OnTriggerExit ( Collider obj  ){
-	thedoor.Play("close");
-}
+		if (audioSource)
+			audioSource.Play();
+	}
+
+	void OnTriggerExit ( Collider obj  )
+	{
+		thedoor.Play("close");
+
+		if (audioSource)
+			audioSource.Play();
+	}
 }
