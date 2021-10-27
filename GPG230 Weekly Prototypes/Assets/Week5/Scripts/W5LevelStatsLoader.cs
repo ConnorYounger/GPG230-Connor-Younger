386cc6a58fe9ecb0700ac96f15d635fd980b70e4
ConnorYounger <72112139+ConnorYounger@users.noreply.github.com>
2021-10-27 20:18:53 +1100

    Main Menu Implementation

---
+++ GPG230 Weekly Prototypes/Assets/Week5/Scripts/W5LevelStatsLoader.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class W5LevelStatsLoader : MonoBehaviour
+{
+    [System.Serializable]
+    public struct levelRefrences
+    {
+        public TMP_Text rankText;
+        public TMP_Text highScoreText;
+        public TMP_Text highMultiplierText;
+    }
+
+    public levelRefrences[] levels;
+
+    private void Start()
+    {
+        LoadLevelStats();
+    }
+
+    public void LoadLevelStats()
+    {
+        for(int i = 0; i < levels.Length; i++)
+        {
+            PlayerData data = SaveSystem.LoadPlayer();
+
+            if (data.levels.Length > i)
+            {
+                levels[i].highScoreText.text = data.levels[i].playerScore.ToString();
+                levels[i].highMultiplierText.text = data.levels[i].scoreMultiplier.ToString();
+                levels[i].rankText.text = RankString(data.levels[i].playerRank);
+            }
+        }
+    }
+
+    string RankString(int rank)
+    {
+        switch (rank)
+        {
+            case 0:
+                return "--";
+                break;
+            case 1:
+                return "E";
+                break;
+            case 2:
+                return "D";
+                break;
+            case 3:
+                return "C";
+                break;
+            case 4:
+                return "B";
+                break;
+            case 5:
+                return "A";
+                break;
+            case 6:
+                return "S";
+                break;
+            case 7:
+                return "SS";
+                break;
+            default:
+                return null;
+        }
+    }
+
+    public void ResetLevelStats()
+    {
+        for (int i = 0; i < levels.Length; i++){
+            SaveSystem.SavePlayer(new W5ScoreManager(), i);
+        }
+
+        for (int i = 0; i < levels.Length; i++)
+        {
+            PlayerData data = SaveSystem.LoadPlayer();
+
+            if (data.levels.Length < i)
+            {
+                levels[i].highScoreText.text = data.levels[i].playerScore.ToString();
+                levels[i].highMultiplierText.text = data.levels[i].scoreMultiplier.ToString();
+                levels[i].rankText.text = RankString(data.levels[i].playerRank);
+            }
+        }
+    }
+}
